2f5743e42bff0803223d5799b3bc16261f6ac604
Zeynep Canbulat <72697219+zeynepcanbulat@users.noreply.github.com>
2020-12-05 15:49:14 +0300

    Add files via upload

---
+++ alıştırmalar3_7.py
@@ -0,0 +1,22 @@
+liste = []
+kaç = int(input("Kaç sayı girmek istiyorsunuz?: "))
+
+for a in range(0,kaç):
+    a = int(input(" {}. Veriyi giriniz: ".format(a+1)))
+    liste.append(a)
+
+print("liste: {}".format(liste))
+for n in range (1,kaç):
+    for i in range (0,len(liste)-1):
+        if int(liste[i]) > int(liste[i+1]):
+            temp = liste[i+1]
+            liste[i+1] = liste[i]
+            liste[i] = temp
+        a = 0
+        if int(liste[i]) < int(liste [i+1]) :
+            a +=1
+        print(liste)
+    print(liste)
+
+
+        
